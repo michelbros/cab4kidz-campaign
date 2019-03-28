78fb345852e72069eda989904cca8f7fb0cf7e62
sunkig <sunk.ig@aol.com>
2019-03-28 10:10:03 +0100

    la till animation

--- js/init.js
+++ js/init.js
@@ -1,3 +1,34 @@
+window.sr = ScrollReveal();
+  sr.reveal('#nav', {
+    duration: 2000,
+    origin: 'top'
+  });
+
+  sr.reveal('#index-banner', {
+    duration: 2000,
+    origin: 'top'
+  });
+
+  sr.reveal('#left', {
+    duration: 2000,
+    origin: 'left',
+    distance: '400px',
+    viewFactor: 0.2
+  });
+
+  sr.reveal('#right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '400px',
+    viewFactor: 0.2
+  });
+
+  sr.reveal('#bottom', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+  });
+
 (function($){
   $(function(){
 
